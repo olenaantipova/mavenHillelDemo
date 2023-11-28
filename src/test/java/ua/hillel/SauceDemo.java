a2f9d3ddd0d7a2842f9065b8719274113313a9af
Olena Antipova <Olena.Antipova@playtech.com>
2023-11-28 12:14:15 +0200

    HW17

--- src/test/java/ua/hillel/SauceDemo.java
+++ src/test/java/ua/hillel/SauceDemo.java
@@ -2,8 +2,10 @@ package ua.hillel;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -15,14 +17,13 @@ import java.util.Set;
 public class SauceDemo extends BaseTest {
     @Test
     public void login() throws InterruptedException {
+        Wait<WebDriver> driverWait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        String initialUrl = driver.getCurrentUrl();
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
-        Thread.sleep(5000);
         driver.findElement(By.id("login-button")).click();
-        Thread.sleep(5000);
-        WebElement linkedinLink = driver.findElement(By.xpath("//a[@href='https://www.linkedin.com/company/sauce-labs/']"));
-        linkedinLink.click();
-        Thread.sleep(5000);
+        By linkElementLocator = By.xpath("//a[@href='https://www.linkedin.com/company/sauce-labs/']");
+        driverWait.until(ExpectedConditions.visibilityOfElementLocated(linkElementLocator)).click();
         Set<String> windowHandles = driver.getWindowHandles();
         String linkedinWindowHandle = (String) windowHandles.toArray()[1];
         driver.switchTo().window(linkedinWindowHandle);
@@ -32,8 +33,8 @@ public class SauceDemo extends BaseTest {
         String mainWindowHandle = (String) windowHandles.toArray()[0];
         driver.switchTo().window(mainWindowHandle);
         driver.findElement(By.id("react-burger-menu-btn")).click();
-        Thread.sleep(1000);
-        driver.findElement(By.id("logout_sidebar_link")).click();
-
+        By logoutElementLocator = By.id("logout_sidebar_link");
+        driverWait.until(ExpectedConditions.visibilityOfElementLocated(logoutElementLocator)).click();
+        Assert.assertTrue(driver.getCurrentUrl().contains(initialUrl), "Logout didn't happen");
     }
 }
